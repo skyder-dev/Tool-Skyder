614ca45688a8ef2a3f001602a92e6a634d788a67
skyder-dev <167886236+skyder-dev@users.noreply.github.com>
2024-05-31 14:30:07 +0200

    Add files via upload

---
+++ Silent install.bat
@@ -0,0 +1,24 @@
+@echo off
+cls
+color 09
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo ---------------------------------------------------------------------------------------------------------------------                                       
+echo.
+echo                                                INSTALLING IN SILENT MODE...
+echo.
+echo.
+echo ---------------------------------------------------------------------------------------------------------------------
+echo.
+pip install --quiet --no-input -r requirements.txt
+start skyder.bat
---
+++ skyder.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+echo Running Skyder tool...
+python "C:\Users\sl8d8\Desktop\Skyder\Skyder\skyder.py"
+pause
