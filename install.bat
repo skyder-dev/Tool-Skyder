872c5ac273e1a671018182db67668e4f675579d6
skyder-dev <167886236+skyder-dev@users.noreply.github.com>
2024-06-08 00:35:44 +0200

    Update and rename Silent install.bat to install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-color 09
+color 1
 cls
 echo.
 echo.
@@ -15,10 +15,10 @@ echo.
 echo.
 echo ---------------------------------------------------------------------------------------------------------------------                                       
 echo.
-echo                                                INSTALLING IN SILENT MODE...
+echo                                              INSTALLING IN SILENT MODE 1.1
 echo.
 echo.
 echo ---------------------------------------------------------------------------------------------------------------------
 echo.
 pip install --quiet --no-input -r requirements.txt
-start skyder.bat
+start discord.bat
